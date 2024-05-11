700eb00546cb17fa98a41fc1e67a3d2f7f51e6ff
milaadelia16 <168887392+milaadelia16@users.noreply.github.com>
2024-05-11 11:54:47 +0700

    Add files via upload

--- list.php
+++ list.php
@@ -1,17 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+<?php 
+$judul = "Daftar Anggota";
+ob_start()
+?>
 <h1>Daftar Anggota</h1>
-    <?php
-    foreach($anggota as $row): ?>
-    <li>
-        <a href=""><?= $row['Nama']; ?> </a>
-    </li>
-    <?php endforeach ?>
-</body>
-</html>
+<ul>
+    <?php 
+        foreach ($anggota as $row):
+    ?>
+</ul>
+<li>
+    <a href='detail.php?id=<?= $row['Id'] ?>' > <?= $row['Nama'];?> </a>
+</li>
+<?php 
+endforeach
+?>
+
+<?php $isi = ob_get_clean()?>
+<?php include "view/template.php";?>
